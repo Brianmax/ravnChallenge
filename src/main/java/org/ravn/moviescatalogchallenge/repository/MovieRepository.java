02f2bc5d711dd185536b0cb4c88ef6f8a8953275
Brianmax <george.maxi@ucsp.edu.pe>
2024-09-01 18:11:59 -0500

    feature: Additional methods for searching by name and synopsis

--- src/main/java/org/ravn/moviescatalogchallenge/repository/MovieRepository.java
+++ src/main/java/org/ravn/moviescatalogchallenge/repository/MovieRepository.java
@@ -3,5 +3,9 @@ package org.ravn.moviescatalogchallenge.repository;
 import org.ravn.moviescatalogchallenge.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
+    Optional<Movie> findByName(String name);
+    Optional<Movie> findBySynopsis(String synopsis);
 }
