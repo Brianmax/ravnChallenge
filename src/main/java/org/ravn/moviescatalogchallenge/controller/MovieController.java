33bfb4034c18f14a7ba4325ffc38d0756ffb2d1f
Brianmax <george.maxi@ucsp.edu.pe>
2024-09-02 15:44:16 -0500

    feature: Movie controller, get by category and all

--- src/main/java/org/ravn/moviescatalogchallenge/controller/MovieController.java
+++ src/main/java/org/ravn/moviescatalogchallenge/controller/MovieController.java
@@ -4,10 +4,9 @@ import org.ravn.moviescatalogchallenge.aggregate.request.MovieRequest;
 import org.ravn.moviescatalogchallenge.aggregate.response.MovieResponse;
 import org.ravn.moviescatalogchallenge.service.MovieService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -26,5 +25,24 @@ public class MovieController {
             return ResponseEntity.badRequest().body("Movie already exist or categories do not exist");
         }
         return ResponseEntity.ok(movieResponse);
+    }
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllMovies(@RequestParam int page, @RequestParam int size) {
+        List<MovieResponse> movieResponses = movieService.getAllMovies(page, size);
+
+        if (movieResponses.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(movieResponses);
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<?> getMoviesByCategory(@RequestParam String category, @RequestParam int page, @RequestParam int size) {
+        List<MovieResponse> movieResponses = movieService.getMoviesByCategory(category, page, size);
+
+        if (movieResponses.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(movieResponses);
     }
 }
