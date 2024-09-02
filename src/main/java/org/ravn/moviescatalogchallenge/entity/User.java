4d574efb461992528522f1031ae6c09d787192f6
Brianmax <george.maxi@ucsp.edu.pe>
2024-09-02 13:13:49 -0500

    refactor: Type migration userId from Long to Integer

--- src/main/java/org/ravn/moviescatalogchallenge/entity/User.java
+++ src/main/java/org/ravn/moviescatalogchallenge/entity/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
-    private Long userId;
+    private int userId;
     private String email;
     private String password;
 
