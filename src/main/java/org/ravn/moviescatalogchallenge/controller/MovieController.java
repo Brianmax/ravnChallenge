f4b7ae7a70e6e2ee69dd0046e49862cbcf65c797
Brianmax <george.maxi@ucsp.edu.pe>
2024-09-02 13:05:29 -0500

    feature: MovieController(save)

---
+++ src/main/java/org/ravn/moviescatalogchallenge/controller/MovieController.java
@@ -0,0 +1,30 @@
+package org.ravn.moviescatalogchallenge.controller;
+
+import org.ravn.moviescatalogchallenge.aggregate.request.MovieRequest;
+import org.ravn.moviescatalogchallenge.aggregate.response.MovieResponse;
+import org.ravn.moviescatalogchallenge.service.MovieService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movie")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveMovie(@RequestBody MovieRequest movieRequest) {
+        MovieResponse movieResponse = movieService.createMovie(movieRequest);
+
+        if (movieResponse == null) {
+            return ResponseEntity.badRequest().body("Movie already exist or categories do not exist");
+        }
+        return ResponseEntity.ok(movieResponse);
+    }
+}
